05149541f2795efa0acee822489fa39b5db65d12
96qbhy <96qbhy@gmail.com>
2017-11-24 20:01:47 +0800

    添加发布仓库判断

--- src/Commands/RepositoryCommand.php
+++ src/Commands/RepositoryCommand.php
@@ -40,19 +40,30 @@ class RepositoryCommand extends Command
     {
         $name = $this->argument('name');
         $class = $name . 'Repository';
-        $model = $this->argument('model') ? $this->argument('model') : '\\App\\Models\\' . $name;
-        $model_name = $this->argument('model_name') ? $this->argument('model_name') : $name;
-        $cache_prefix = $this->argument('cache_prefix') ? $this->argument('cache_prefix') : $name . '_id:';
 
-        $content = File::get(__DIR__ . '/../../tmp/ExampleRepository');
-        $content = str_replace("{class}", $class, $content);
-        $content = str_replace("{model}", $model, $content);
-        $content = str_replace("{model_name}", $model_name, $content);
-        $content = str_replace("{cache_prefix}", $cache_prefix, $content);
+        if (File::exists(app_path("Repositories/$class.php"))) {
 
-        File::put(app_path("Repositories/$class.php"), $content);
+            $this->error("$class exists !");
 
-        $this->info('Create repository success !');
+        } else {
+
+            $model = $this->argument('model') ? $this->argument('model') : '\\App\\Models\\' . $name;
+            $model_name = $this->argument('model_name') ? $this->argument('model_name') : $name;
+            $cache_prefix = $this->argument('cache_prefix') ? $this->argument('cache_prefix') : $name . '_id:';
+
+            $content = File::get(__DIR__ . '/../../tmp/ExampleRepository');
+            $content = str_replace("{class}", $class, $content);
+            $content = str_replace("{model}", $model, $content);
+            $content = str_replace("{model_name}", $model_name, $content);
+            $content = str_replace("{cache_prefix}", $cache_prefix, $content);
+
+            File::put(app_path("Repositories/$class.php"), $content);
+
+            $this->publishBaseRepository();
+
+            $this->info('Create repository success !');
+        }
+
     }
 
     public function publishBaseRepository()
@@ -66,4 +77,5 @@ class RepositoryCommand extends Command
             $this->info('Create base repository file success !');
         }
     }
+
 }
