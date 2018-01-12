6abc16aada329120ed3df3b6dbebf70ea766db2b
96qbhy <96qbhy@gmail.com>
2018-01-12 17:31:00 +0800

    更新getDataFromCache方法

--- src/Repository.php
+++ src/Repository.php
@@ -147,10 +147,7 @@ abstract class Repository implements RepositoryInterface
         $data = $this->getTaggedCache()->get($cache_key);
 
         if (is_null($data)) {
-            $model = $id === $model ? $this->find($id) : $model;
-            if (is_null($model)) {
-                return null;
-            }
+            $model = $id === $model ? $this->findOrFail($id) : $model;
             $data = $this->format($model);
             $this->getTaggedCache()->forever($cache_key, $data);
         }
